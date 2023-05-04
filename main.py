ab7f1730ea9437dbec7a3fddceb775f006fd334e
SvetoCopy <65361271+SvetoCopy@users.noreply.github.com>
2023-05-04 15:42:13 +0300

    Debug

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-from Littles_method import Main_method
-import matplotlib.pyplot as plt
-import json
 from Inp_Rea import Read_Json
-
-
-def index_to_coord(data, points):
-    l_x = []
-    l_y = []
-    for i in range(len(points)):
-        for j in range(len(data)):
-
-            if points[i][0] == data[j]["id"]:
-                l_x.append([data[j]["x"], data[j]["y"]])
-            if points[i][1] == data[j]["id"]:
-                l_y.append((data[j]["x"], data[j]["y"]))
-    return l_x, l_y
+from Littles_method import Main_Method
+import matplotlib.pyplot as plt
 
 
 def main():
-    file = Read_Json("file_kt_1.json", "")
-    mat = Main_method(1, file.preparation())
-    coords = mat.solution_cycle()[0]
-    l_x, l_y = index_to_coord(file.data, coords)
-    plt.plot(l_x, l_y)
+    fig, ax = plt.subplots()
+    geom = Read_Json("file_kt_1.json")
+    geom.draw(ax)
+    geom.preparation()
+    res = Main_Method("", geom.matrix, 0)
+    draw_mat = geom.path_matrix
+    for path in res.solution_cycle()[0]:
+        draw_mat[int(path[0])][int(path[1])].draw(ax)
+    ax.set_aspect('equal')
     plt.show()
 
 
